c393febcab422ded781b79a0de1367c44f5ba664
Joshua Sierles <joshua@chatterbug.com>
2021-08-10 19:50:58 +0200

    use the recipe app as the target

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  duration: "2s"
+  duration: "10s"
 };
 
 export default function () {
-  http.get("http://letrasflamencas.es/lyrics/13940")
+  http.get("https://cookherenow.com")
 }
