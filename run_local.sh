fca79b7f087b33aae3c146ac5b4f865d2af52008
Joshua Sierles <joshua@chatterbug.com>
2021-08-22 21:48:48 +0200

    make number of users and the URL configurable via the environment

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in `fly ips private | grep -v IP | awk -F" " '{print $3}'`
-  do ssh -oStrictHostKeyChecking=no root@$i "sh run.sh" &
+  do ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i "sh run.sh" &
 done
 
 wait
--- test.js
+++ test.js
@@ -2,9 +2,10 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  duration: "10s"
+  duration: "10s",
+  vus: __ENV.VUS || 1
 };
 
 export default function () {
-  http.get("https://cookherenow.com")
+  http.get(__ENV.URL)
 }
